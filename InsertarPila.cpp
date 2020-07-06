899b355022b8fa86a3ae57c65ebd45db73f81521
Viceroy <poeviceroy@hotmail.com>
2020-07-05 19:38:45 -0500

    Insertar Y Eliminar datos de la Pila

--- InsertarPila.cpp
+++ InsertarPila.cpp
@@ -9,24 +9,47 @@ Nodo *siguiente;
 	
 };
 
-void agregarPila(Nodo *&, int);
+void agregarPila(Nodo *&, int);  //Prototipos
+void eliminarPila(Nodo *&, int &);  //de función.
+
 
 int main(){
-	Nodo *pila=NULL; 
-	int n1, n2;
+	Nodo *pila=NULL; //igualar pila a un valor vacío. 
+	int dato;
 	
 	cout<<"digite un número para apilar: "<<endl;
-	cin>>n1;
-	agregarPila(pila, n1);
+	cin>>dato;
+	agregarPila(pila, dato);
 	
 		cout<<"digite otro número para apilar: "<<endl;
-	cin>>n2;
-		agregarPila(pila, n2);
+	cin>>dato;
+		agregarPila(pila, dato);
+		
+	cout<<"digite otro número para apilar: "<<endl;
+	cin>>dato;
+		agregarPila(pila, dato);
+	cout<<"Sacando los elementos de la pila: ";
+	
+			cout<<"digite otro número para apilar: "<<endl;
+	cin>>dato;
+		agregarPila(pila, dato);
+		
+	while (pila != NULL)//Mientras no sea el final...
+	{
+		eliminarPila(pila, dato);
+		if (pila != NULL){//hay más elementos
+		cout<<dato<<" , ";
+		}
+		else
+		{
+			cout<<".";
+		}
+	}
 	
 	getch();
 	return 0;
 }
- void agregarPila(Nodo *&pila, int n)
+ void agregarPila(Nodo *&pila, int n) //Agregar datos
  {
  	Nodo *nuevoNodo = new Nodo();
  	nuevoNodo -> dato = n; 
@@ -34,4 +57,11 @@ int main(){
  	pila=nuevoNodo;
  	
  	cout<<n<<" agregado correctamente a la pila"<<endl;
+ }
+ 
+ void eliminarPila(Nodo *& pila, int &n){ //Eliminar Datos
+ 	Nodo *aux=pila;
+ 	n = aux ->dato;
+ 	pila = aux ->siguiente;
+ 	delete aux; 
  }
